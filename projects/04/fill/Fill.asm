54419ed89f5ef266cb81cb930a729ebb2bc0a7c7
Richard Artoul <richardartoul@gmail.com>
2015-08-02 19:53:26 -0700

    [Feature] Wrote the fill assembly program that turns the screen black when a key is pressed, and turns it white otherwise

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -9,3 +9,97 @@
 // program clears the screen, i.e. writes "white" in every pixel.
 
 // Put your code here.
+
+//set number of 16bit blocks to color
+@8192
+D=A
+@screenSize
+M=D
+
+(MAINLOOP)
+  //If key is pressed, blacken screen
+  @KBD
+  D=M
+  @BLACKSCREEN
+  D;JNE
+
+  //else whiten screen
+  @WHITESCREEN
+  0;JMP
+
+(BLACKSCREEN)
+  //counter to know when we've hit every pixel
+  @i
+  M=0
+
+  (BLACKLOOP)
+    //check if screen is completely black
+    @i
+    D=M
+    @screenSize
+    D=D-M
+    //if it is, go back to main loop
+    @MAINLOOP
+    D;JEQ
+
+    //else keep painting black
+
+    //block will point to sequence of 16 pixels that will be blacked out
+    @SCREEN
+    D=A
+    @i
+    D=D+M
+    @block
+    M=D
+
+    //-1 is represented as all 1's in 2's complement i.e BLACK
+    @block
+    A=M
+    M=-1
+
+    //increment i
+    @i
+    M=M+1
+
+    //repeat loop
+    @BLACKLOOP
+    0;JMP
+
+
+(WHITESCREEN)
+  //counter to know when we've hit every pixel
+  @i
+  M=0
+
+  (WHITELOOP)
+    //check if screen is completely white
+    @i
+    D=M
+    @screenSize
+    D=D-M
+    //if it is, go back to main loop
+    @MAINLOOP
+    D;JEQ
+
+    //else keep painting white
+
+    //block will point to sequence of 16 pixels that will be blacked out
+    @SCREEN
+    D=A
+    @i
+    D=D+M
+    @block
+    M=D
+
+    //0 corresponds to white
+    @block
+    A=M
+    M=0
+
+    //increment i
+    @i
+    M=M+1
+
+    //repeat loop
+    @WHITELOOP
+    0;JMP
